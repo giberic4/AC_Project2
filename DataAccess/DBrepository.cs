881dfb66beedab7bbba4888e77116eb03396d1e5
J R <john203@revature.net>
2023-03-09 16:31:26 -0600

    updated Services and DataAccess

--- DataAccess/DBrepository.cs
+++ DataAccess/DBrepository.cs
@@ -1,5 +1,20 @@
-﻿namespace DataAccess;
-public class Class1
-{
+﻿using Models;
 
+namespace DataAccess;
+public class DBRepository : IRepository
+{
+    public User AddUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Item> GetAllItems()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<User> GetAllUsers()
+    {
+        throw new NotImplementedException();
+    }
 }
