f6f5864f226e99e7d21f68bdaa4b8f0e72c30733
ArturGevorgyan007 <92591500+ArturGevorgyan007@users.noreply.github.com>
2023-03-10 07:52:53 -0800

    Delete Secrets.cs

--- DataAccess/Secrets.cs
+++
@@ -1,5 +0,0 @@
-namespace DataAccess;
-internal class Secrets{
-    private const string _connectionString = "Server=tcp:230206net-p2-server.database.windows.net,1433;Initial Catalog=TeamB;Persist Security Info=False;User ID=teamB;Password=@n!ma1Cr0$$ing;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-    public static string getConnectionString() => _connectionString;
-}
