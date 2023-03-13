a7389629c4c5178e530bd3bedebb869114e0291b
Artur <arturgevorgyan87@yahoo.com>
2023-03-13 14:45:59 -0700

    UserLogin

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -3,44 +3,45 @@ using Models;
 
 namespace Services;
 public class UserServices{
-    IRepository _iRepo;
-    UserServices(IRepository iRepo){
+    private IRepository _iRepo;
+    public UserServices(IRepository iRepo){
         _iRepo = iRepo;
     }
 
     public List<User>? GetUsers(){
         return _iRepo.GetAllUsers();
     }
-    public void Login(){
-
+    public bool UserLogin(User user){
+        Console.WriteLine("@@");
+        return _iRepo.UserLogin(user);
     }
 
-    public void CreateAccount(){
-        try{
-            Console.Write("Please Enter Your First Name: ");
-            string fname = Console.ReadLine()!;
+    // public void CreateAccount(){
+    //     try{
+    //         Console.Write("Please Enter Your First Name: ");
+    //         string fname = Console.ReadLine()!;
 
-            Console.Write("Please Enter Your Last Name: ");
-            string lname = Console.ReadLine()!;
+    //         Console.Write("Please Enter Your Last Name: ");
+    //         string lname = Console.ReadLine()!;
 
-            Console.Write("Please Create Your Username: ");
-            string uName = Console.ReadLine()!;
+    //         Console.Write("Please Create Your Username: ");
+    //         string uName = Console.ReadLine()!;
 
-            Console.Write("Please Create A Password: ");
-            string pwd = Console.ReadLine()!;
+    //         Console.Write("Please Create A Password: ");
+    //         string pwd = Console.ReadLine()!;
 
-            User newUser = new ();
-            newUser.FirstName = fname;
-            newUser.LastName = lname;
-            newUser.Username = uName;
-            newUser.Password = pwd;
-            newUser.Wallet = 1000;
-            _iRepo.AddUser(newUser);
+    //         User newUser = new ();
+    //         newUser.FirstName = fname;
+    //         newUser.LastName = lname;
+    //         newUser.Username = uName;
+    //         newUser.Password = pwd;
+    //         newUser.Wallet = 1000;
+    //         _iRepo.AddUser(newUser);
 
-        }
-        catch(Exception e){
-            Console.Write("Error When Creating Account: {0}", e);
-        }
-    }
+    //     }
+    //     catch(Exception e){
+    //         Console.Write("Error When Creating Account: {0}", e);
+    //     }
+    // }
     
 }
