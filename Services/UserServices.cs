8594bc5cf6f40ceaed8ad674f309e745171765fd
Artur <arturgevorgyan87@yahoo.com>
2023-03-14 09:55:24 -0700

    Removed conflict code

--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -18,33 +18,4 @@ public class UserServices{
     public User ViewPersonalInventory(User user) {
         return _iRepo.ViewPersonalInventory(user);
     }
-
-    // public void CreateAccount(){
-    //     try{
-    //         Console.Write("Please Enter Your First Name: ");
-    //         string fname = Console.ReadLine()!;
-
-    //         Console.Write("Please Enter Your Last Name: ");
-    //         string lname = Console.ReadLine()!;
-
-    //         Console.Write("Please Create Your Username: ");
-    //         string uName = Console.ReadLine()!;
-
-    //         Console.Write("Please Create A Password: ");
-    //         string pwd = Console.ReadLine()!;
-
-    //         User newUser = new ();
-    //         newUser.FirstName = fname;
-    //         newUser.LastName = lname;
-    //         newUser.Username = uName;
-    //         newUser.Password = pwd;
-    //         newUser.Wallet = 1000;
-    //         _iRepo.AddUser(newUser);
-
-    //     }
-    //     catch(Exception e){
-    //         Console.Write("Error When Creating Account: {0}", e);
-    //     }
-    // }
-    
 }
